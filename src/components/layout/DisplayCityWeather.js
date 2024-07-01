ee244b2a69c2e8767bfc3590c73d52b0a0e017f5
RajanSerialcom <rajan.serialcominfotech@gmail.com>
2024-07-01 14:12:36 +0530

    updated backdrops

--- src/components/layout/DisplayCityWeather.js
+++ src/components/layout/DisplayCityWeather.js
@@ -4,9 +4,16 @@ import bgStyles from "@/styles/backdrops.module.css";
 import cityStyles from "@/styles/city.module.css";
 
 import logo1 from "../../../public/assets/icons/humidity.svg";
-import logo2 from "../../../public/assets/icons/sunrise.svg";
+import logo2 from "../../../public/assets/icons/sunset.svg";
 import logo3 from "../../../public/assets/icons/wind.svg";
-import logo4 from "../../../public/assets/icons/sunset.svg";
+import logo4 from "../../../public/assets/icons/sunrise.svg";
+
+import bgRain from "../../../public/assets/backdrops/Rain.jpg";
+import bgClear from "../../../public/assets/backdrops/Clear.jpeg";
+import bgClouds from "../../../public/assets/backdrops/Clouds.jpg";
+import bgSnow from "../../../public/assets/backdrops/Snow.jpg";
+import bgThunderstorm from "../../../public/assets/backdrops/Thunderstorm.jpg";
+import Image from "next/image";
 
 export default function DisplayCityWeather({
   temp,
@@ -23,6 +30,7 @@ export default function DisplayCityWeather({
   country,
   icon,
 }) {
+  const bgImg = getBgImage(weatherCondition);
   return (
     <Container className={cityStyles.container} maxWidth="xl" disableGutters>
       <Box
@@ -35,20 +43,29 @@ export default function DisplayCityWeather({
         }}
       >
         <Box
-          className={`${bgStyles.main_bg} shadow_4 ${cityStyles.temp_container} entry_animation`}
+          className={`shadow_4 ${cityStyles.temp_container} entry_animation`}
           sx={{
             maxHeight: { ss: 600, xs: 600, sm: 400 },
             flexDirection: { ss: "column", xs: "column", sm: "row" },
             justifyContent: { ss: "center", xs: "center", sm: "space-between" },
-            backgroundSize: { ss: "200% 130%", xs: "200% 130%", sm: "cover" },
             py: { ss: 4, xs: 4, sm: 2 },
           }}
         >
+          <Image
+            width={1000}
+            height={500}
+            className={bgStyles.main_bg}
+            src={bgImg}
+            priority
+            alt="weather background"
+          />
+          <Box className={bgStyles.overlay} />
           <Stack
             gap={1}
             direction={"column"}
             justifyContent={"end"}
             alignItems={{ ss: "center", xs: "center", sm: "start" }}
+            zIndex={1}
           >
             <Avatar
               src={icon}
@@ -76,6 +93,7 @@ export default function DisplayCityWeather({
             >{`${cityName}, ${country}`}</Typography>
           </Stack>
           <Stack
+            zIndex={1}
             direction={"column"}
             gap={1}
             justifyContent={"end"}
@@ -89,7 +107,7 @@ export default function DisplayCityWeather({
         </Box>
         <Box
           key={temp}
-          className="entry_animation dealy_3"
+          className="entry_animation dealy_3 shadow_1"
           sx={{
             maxWidth: { ss: 1000, xs: 1000, sm: 1000, md: 450 },
             borderRadius: 4,
@@ -106,7 +124,7 @@ export default function DisplayCityWeather({
           </Box>
         </Box>
         <Box
-          className="entry_animation dealy_3"
+          className="entry_animation dealy_3 shadow_1"
           sx={{
             flexBasis: "30%",
             borderRadius: 4,
@@ -182,4 +200,12 @@ function ForecastBox({ icon, temp, w1, w2, date }) {
       </Stack>
     </Box>
   );
+}
+
+function getBgImage(conditon) {
+  if (conditon == "Rain") return bgRain;
+  if (conditon == "Clouds") return bgClouds;
+  if (conditon == "Snow") return bgSnow;
+  if (conditon == "Thunderstorm") return bgThunderstorm;
+  else return bgClear;
 }
